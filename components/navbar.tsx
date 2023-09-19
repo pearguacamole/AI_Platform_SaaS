8831a7c871501bab447896cdbd7ed47a00896448
pearguacamole <100153737+pearguacamole@users.noreply.github.com>
2023-09-20 00:51:18 +0530

    implement sidebar funtionalities

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
+import MoblileSidebar from "./mobile-sidebar";
 const Navbar = () => {
     return(
         <div className="flex items-center p-4">
-            <Button variant="ghost" size="icon" className="md:hidden">
-                <Menu/>
-            </Button>
+            <MoblileSidebar/>
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/"/>
             </div>
